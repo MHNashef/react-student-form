d8b7dd1f8ae13aec905952360577853d77108509
Hisham Nashef <hishamnshf@gmail.com>
2021-06-07 19:50:09 +0300

    initialized Modal component

--- src/DAL/api.js
+++ src/DAL/api.js
@@ -1,43 +1,56 @@
 const students = [
-    {
-        username: "Hisham",
-        email: "hisham@gmail.com",
-        address: "17551 Wayne Ave",
-        course: "JavaScript",
-        gender: "Male"
-    },
+  {
+    username: "Hisham",
+    email: "hisham@gmail.com",
+    address: "17551 Wayne Ave",
+    course: "JavaScript",
+    gender: "Male",
+  },
 
-    {
-        username: "Yakov Kassa",
-        email: "yakov@gmail.com",
-        address: "Netanya, Israel",
-        course: "MySQL",
-        gender: "Male"
-    },
+  {
+    username: "Yakov Kassa",
+    email: "yakov@gmail.com",
+    address: "Netanya, Israel",
+    course: "MySQL",
+    gender: "Male",
+  },
 
-    {
-        username: "Yema",
-        email: "yema@gmail.com",
-        address: "Somewhere in Israel",
-        course: "React JS",
-        gender: "Male"
-    },
+  {
+    username: "Yema",
+    email: "yema@gmail.com",
+    address: "Somewhere in Israel",
+    course: "CSS",
+    gender: "Female",
+  },
+  {
+    username: "Eliezer",
+    email: "eliezer@gmail.com",
+    address: "Somewhere in Israel",
+    course: "HTML",
+    gender: "Male",
+  },
+  {
+    username: "Marta",
+    email: "marta@gmail.com",
+    address: "Somewhere in Israel",
+    course: "Python",
+    gender: "Female",
+  },
 ];
 
-
 function addStudent(loginData) {
-    const studentData = {};
+  const studentData = {};
 
-    for (const input in loginData) {
-        studentData[input] = loginData[input].value;
-    }
+  for (const input in loginData) {
+    studentData[input] = loginData[input].value;
+  }
 
-    students.push({...studentData});
-    console.log(students);
+  students.push({ ...studentData });
+  console.log(students);
 }
 
 export function getStudents() {
-    return students;
+  return students;
 }
 
 export default addStudent;
