b6d8aacd991c998c1b8ee4aa805106c86358e140
Hisham Nashef <hishamnshf@gmail.com>
2021-06-23 22:49:49 +0300

    fixed bug which caused program to crash when clicking on table row

--- src/components/CardDisplay.jsx
+++ src/components/CardDisplay.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import getStudents from "../DAL/api";
+import { Card } from "react-bootstrap";
 
-export default function Card({ id, students, card }) {
-    const studentToDisplay = students.filter(studentObj => (studentObj.id == id));
-    console.log(students);
-    console.log(studentToDisplay[0]);
-    console.log(id);
+export default function CardDisplay({ id, students, card }) {
+  const studentToDisplay = students.filter((studentObj) => studentObj.id == id);
+
   return (
     <>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "18rem" }} bg="dark" text="light">
         <Card.Body>
           <Card.Title>Student Info</Card.Title>
           <Card.Text>studentToDisplay.username</Card.Text>
--- src/components/StudentPage.jsx
+++ src/components/StudentPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AddStudentModal from "./AddStudentModal";
-import Card from "./Card";
+import CardDisplay from "./CardDisplay";
 import { getStudents } from "../DAL/api";
 import { Container, Table, Button, Row, Col } from "react-bootstrap";
 
@@ -68,9 +68,9 @@ export default function StudentPage() {
       <Button variant="primary" onClick={sort}>
         {sortButton}
       </Button>
-      {/* {
-          card.show && <Card id={card.id} students={students} card={card} />
-      } */}
+      {
+          card.show && <CardDisplay id={card.id} students={students} card={card} />
+      }
       <AddStudentModal show={show} handleClose={handleClose} />
     </Container>
   );
